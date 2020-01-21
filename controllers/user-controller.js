ac656dfb56ff93c881f43f8ed0d7231d93d5649f
moose <mr.rammo@gmail.com>
2020-01-21 18:08:30 -0500

    create user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,101 @@
+const User = require('../models/user-model');
+const Post = require('../models/post-model');
+const Follow = require('../models/follow-model');
+const Feed = require('../models/feed-model');
+
+/*
+  /u/home
+  Render the authenticated user's home page
+*/
+exports.renderHome = async (req, res, next) => {
+	return res.render('home', {user: req.user});
+};
+
+/*
+  /u/users
+  Render the users page with all stored users
+*/
+exports.renderUsers = async (req, res, next) => {
+	let error = null;
+
+	// get all users
+	let users = await User.find()
+		.catch(error_ => {
+      error = error_;
+    });
+
+	if (error) {
+		return next(error);
+  }
+
+	res.render('users', {user: req.user, users: users});
+};
+
+/*
+  /:profileId
+  Find user profile by profileId and render profile page with follow information
+*/
+exports.renderProfileByProfileId = async (req, res, next) => {
+	let error = null;
+  let user = req.user;
+
+  // find user
+	let profile = await User.findById(req.params.profileId)
+		.catch(error_ => {
+      error_ = error;
+    });
+
+	if (!profile) {
+    // No user
+    return res.redirect('/u/home');
+  }
+
+	if (error) {
+    return next(error);
+  }
+
+	// check follow status between users, fast because of index
+	let following = await Follow.findOne({ user: profile._id, follower: user._id })
+		.catch(error_ => {
+      error = error_;
+    })
+    .map(document => {
+      // true: following relation found
+      // false: following relation not found
+      return document != null;
+    });
+
+	if (error) {
+		return next(error);
+  }
+
+	// get followers and followees
+	// TODO: move this to API ?
+	let followers = await Follow.find({user: profile._id})
+		.populate('follower')
+		.catch(error_ => {
+      error = error_;
+    });
+
+	if (error) {
+		return next(error);
+  }
+
+	let followees = await Follow.find({follower: profile._id})
+		.populate('user')
+		.catch(error_ => {
+      error = error_;
+    });
+
+	if (error) {
+		return next(error);
+  }
+
+	res.render('profile', {
+		user,
+		profile,
+		following,
+		followers,
+		followees
+	});
+}
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,86 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user-model');
-const Post = require('../models/post-model');
-const Follow = require('../models/follow-model');
-const Feed = require('../models/feed-model');
+const userController = require('../controllers/user-controller');
 
-// gets user's posts temporarily
-// TODO: make this get user's newsfeed
-// render user newsfeed
-router.get('/home', async (req, res, next) => {
-	return res.render('home', { user: req.user });
-});
-
-router.get('/users', async (req, res, next) => {
-	let error = null;
-	let users = null;
-
-	// get all users
-	await User.find()
-		.then(u => users = u)
-		.catch(e => error = e);
-
-	if (error)
-		return next(error);
-
-	res.render('users', { user: req.user, users: users })
-});
-
-// render user profile
-router.get('/:profileId', async (req, res, next) => {
-	let error = null;
-
-	// find user profile
-	let profile = null;
-	await User.findById(req.params.profileId)
-		.then(p => profile = p)
-		.catch(err => error = err);
-
-	// check if user exists, otherwise go home
-	if (!profile)
-		return res.redirect('/u/home');
-	if (error)
-		return next(error);
-
-	// check follow status between users, fast because of index
-	let following = false;
-	await Follow.findOne({ user: profile._id, follower: req.user._id })
-		.then(f => following = (f != null))
-		.catch(e => error = e);
-
-	console.log(following);
-
-	if (error)
-		return next(error);
-
-	// get followers and followees
-	// TODO: move this to API ?
-	let followers = null;
-	await Follow.find({ user: profile._id })
-		.populate('follower')
-		.then(f => followers = f)
-		.catch(e => error = e);
-
-	if (error)
-		return next(error);
-
-	let followees = null;
-	await Follow.find({ follower: profile._id })
-		.populate('user')
-		.then(f => followees = f)
-		.catch(e => error = e);
-
-	if (error)
-		return next(error);
-
-	res.render('profile', {
-		user: req.user,
-		profile,
-		following,
-		followers,
-		followees
-	});
-});
+router.get('/home', userController.renderHome);
+router.get('/users', userController.renderUsers
+router.get('/:profileId', userController.renderProfileByProfileId);
 
 module.exports = router;
